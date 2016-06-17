b23405746d4627ef25becc5a3dd7409e529ca23b
aashraya <aashraya_p@hotmail.com>
2016-06-17 15:08:27 -0500

    changed some bugs

--- reversal.rb
+++ reversal.rb
@@ -1,8 +1,10 @@
 def reversal (string) #method to reverse any string without using .reverse
 	separated_string=string.split('')
+	i=1
 	while i< separated_string.size/2
-		separated_string[i], separated_string[-i]=separated_string[-i],separated_string[i]
+		separated_string[i-1], separated_string[-i]=separated_string[-i],separated_string[i-1]
 		separated_string.join(' ')
+		i+=1
 	end
 end
 
